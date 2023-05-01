19b091f21e5a0deae6fb615175d6c5dd0b531ec9
Eli Moshe <moshebogo@gmail.com>
2023-05-01 13:36:25 -0400

    4 methods working

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -6,25 +6,35 @@ function reverseString(str) {
 }
 
 function reverseString2(str) {
-  let newstring = ''
-  for (let i = str.length-1; i >= 0; i--) {
-       newstring += str[i]
-  }
-  return newstring
+  return str.split('').reverse().join('')
 }
+
+function reverseString3(str) {
+  let newString = ''
+  for (let i = str.length-1; i >= 0; i--) {
+       newString += str[i]
+  }
+  return newString
+}
+
+function reverseString4(str) {
+  return str === '' ? '' : reverseString4(str.substr(1)) + str.charAt(0)
+}
+
+console.log(reverseString4('1234'))
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
-  console.log("=>", reverseString2("hi"));
+  console.log("=>", reverseString4("hi"));
 
   console.log("");
 
   console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString2("catbaby"));
+  console.log("=>", reverseString4("catbaby"));
 }
 
-module.exports = reverseString2;
+module.exports = reverseString4;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
